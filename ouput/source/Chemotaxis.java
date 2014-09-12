492f86fbaddc7e5383478ab9d98e3c58a38a3489
zachooz <hoffking@gmail.com>
2014-09-11 18:00:28 -0700

    update

--- ouput/source/Chemotaxis.java
+++ ouput/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria BacteriaHolder[] = new Bacteria[10];
 
-void setup(){
+public void setup(){
   size(500,500);
   build();
   background(0,0,0);
@@ -26,7 +42,7 @@ public class Bacteria{
       rgb[i] = (int) (Math.random() *256);
     }
   }
-  void move(){
+  public void move(){
     if(newPointX==x || newPointY==y){
       newPointX = x + (int) (Math.random()*101 -50);
       newPointY = y + (int) (Math.random()*101 -50);
@@ -59,16 +75,25 @@ public class Bacteria{
     ellipse(x, y, 10, 10);
   }
 }
-void build(){
+public void build(){
   for(int i = 0; i<BacteriaHolder.length-1;i++){
     BacteriaHolder[i] = new Bacteria();
   }
 }
 
-void draw(){
+public void draw(){
   fill(0,0,0,40);
   rect(0 ,0 , 1000, 1000);
   for(int i = 0; i<BacteriaHolder.length-1;i++){
     BacteriaHolder[i].move();
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
